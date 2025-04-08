9c92d69f8e20f1da8a8e85155ccf535f47ed1baa
ForteHealthcare <carol@fortehealthcare.com.au>
2025-04-09 08:41:42 +1000

    Update route.ts

--- app/api/getEvents/route.ts
+++ app/api/getEvents/route.ts
@@ -1,5 +1,5 @@
 export async function GET() {
-  const res = await fetch('https://script.google.com/macros/s/YOUR_SCRIPT_ID_HERE/exec');
+  const res = await fetch('https://script.google.com/macros/s/AKfycbwMHKyjOQLma7mtYkHsVaAetb7cSaX31G1Ow-q1FzcWXb0ceNwgx3A8vJjlH9ydOpk/exec');
   const data = await res.json();
   return new Response(JSON.stringify(data), { status: 200 });
 }
